061524c7f9bcea6d5304dbc44dcf3a0e36f6560b
GaEun1216 <gani6382@gmail.com>
2024-05-16 16:38:34 +0900

    init OMD (only my diary)

---
+++ src/main/java/com/sparta/omd/OmdApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.omd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OmdApplication.class, args);
+    }
+
+}
